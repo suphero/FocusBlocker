7f717f44b97d2e986b482552401bbe6b39131f30
Harun Sokullu <harunsokullu@gmail.com>
2023-01-14 23:11:23 +0300

    dynamic icon

--- background.js
+++ background.js
@@ -20,3 +20,27 @@ chrome.runtime.onInstalled.addListener(function (details) {
     chrome.storage.local.set({ enabled: false, websites: "facebook.com\ntwitter.com\ninstagram.com\nyoutube.com\nwhatsapp.com" });
   }
 });
+
+chrome.storage.onChanged.addListener(function (changes) {
+  if (changes.enabled) {
+    if (changes.enabled.newValue) {
+      chrome.action.setIcon({
+        path: {
+          "16": "images/enabled16.png",
+          "32": "images/enabled32.png",
+          "48": "images/enabled48.png",
+          "128": "images/enabled128.png"
+        }
+      });
+    } else {
+      chrome.action.setIcon({
+        path: {
+          "16": "images/disabled16.png",
+          "32": "images/disabled32.png",
+          "48": "images/disabled48.png",
+          "128": "images/disabled128.png"
+        }
+      });
+    }
+  }
+});
