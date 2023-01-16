2df90e96f96c692f05e114ed1f26f4bee5026be0
Harun Sokullu <harunsokullu@gmail.com>
2023-01-16 23:45:50 +0300

    refactor popup

--- popup.js
+++ popup.js
@@ -1,15 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-  chrome.storage.local.get(["enabled", "websites"]).then((result) => {
-    document.getElementById("enabled").checked = result.enabled || false;
-    document.getElementById("websites").innerHTML = result.websites || '';
+const onSave = () => {
+  const enabled = document.getElementById("enabled").checked;
+  const websites = document.getElementById("websites").value;
+  chrome.storage.local.set({ enabled, websites }, () => {
+    window.close();
   });
+}
 
-  document.getElementById("save").addEventListener("click", function () {
-    const enabled = document.getElementById("enabled").checked;
-    const websites = document.getElementById("websites").value;
-    chrome.storage.local.set({ enabled, websites }, function () {
-      console.log("Saved");
-      window.close();
-    });
-  });
-});
+const init = async () => {
+  const { enabled, websites } = await chrome.storage.local.get(["enabled", "websites"]);
+  document.getElementById("enabled").checked = enabled || false;
+  document.getElementById("websites").innerHTML = websites || '';
+  document.getElementById("save").addEventListener("click", onSave);
+}
+
+document.addEventListener("DOMContentLoaded", init);
